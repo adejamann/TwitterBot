9d9e2e785b849e7e4e845f56b0435354f396f8e5
jxu645 <113573406+jxu645@users.noreply.github.com>
2022-11-04 21:33:19 -0400

    Update TwitterBot.js

--- TwitterBot.js
+++ TwitterBot.js
@@ -106,45 +106,84 @@ function likepost() {
 	});
 }
 
-//Tweet random graphic design images from images folder
-//Generates an image from 
-const randomFromArray = (images) => {
-	return images[Math.floor(Math.random() * images.length)];
-  } 
-// Pull out image from image.js
-const uploadRandomImage = (images) => {
-	console.log('opening an image...');
-	const randomImage = randomFromArray(images);
-	const imagePath = path.join(__dirname, '/images/' + randomImage.file);
-	const imageData = fs.readFileSync(imagePath, {encoding: 'base64'});
-	// Upload image to post
-	T.post('media/upload', {media_data: imageData}, (err, data, response) => {
-		console.log('uploading an image...');
-		// Print out error if an error occurs
-		if (err){
-			console.log('error:', err);
-		} else {
-			console.log('adding description...');
-			const image = data;
-			//Post the random image
-			T.post('media/metadata/create', {
-				media_id: data.media_id_string,
-				alt_text: {
-					text: randomImage.altText
-				}            
-			}, (err, data, response) => {
-				console.log('tweeting...');
-				T.post('statuses/update', {
-					status: randomImage.text,
-					media_ids: new Array(image.media_id_string)
-				}, (err, data, response) => {
-					if (err){
-						console.log('error:', err);
-					}
-				});
-			});
-		}
-	});
+const randomFromArray = (arr) => {
+    /* Helper function for picking a random item from an array. */
+
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+const tweetRandomImage = () => {
+    /* First, read the content of the images folder. */
+
+    fs.readdir(__dirname + '/images', (err, files) => {
+        if (err){
+            console.log('error:', err);
+            return;
+        } else {
+            let images = [];
+
+            files.forEach((f) => {
+                images.push(f);
+            });
+
+            /* Then pick a random image. */
+
+            console.log('opening an image...');
+
+            const imagePath = path.join(__dirname, '/images/' + randomFromArray(images)),
+                  imageData = fs.readFileSync(imagePath, { encoding: 'base64' });
+
+            /* Upload the image to Twitter. */
+
+            console.log('uploading an image...', imagePath);
+
+            T.post('media/upload', { media_data: imageData }, (err, data, response) => {
+                if (err){
+                    console.log('error:', err);
+                } else {
+                    /* Add image description. */
+                    
+                    const image = data;
+                    console.log('image uploaded, adding description...');
+
+                    T.post('media/metadata/create', {
+                        media_id: image.media_id_string,
+                        alt_text: {
+                            text: 'Describe the image'
+                        }            
+                    }, (err, data, response) => {
+
+                        /* And finally, post a tweet with the image. */
+
+                        T.post('statuses/update', {
+                            // status: 'Optional tweet text.',
+                            media_ids: [image.media_id_string]
+                        },
+                        (err, data, response) => {
+                            if (err){
+                                console.log('error:', err);
+                            } else {
+                                console.log('posted an image!');
+
+                                /*
+                                    After successfully tweeting, we can delete the image.
+                                    Keep this part commented out if you want to keep the image and reuse it later.
+                                */
+
+                                // fs.unlink(imagePath, (err) => {
+                                //   if (err){
+                                //     console.log('error: unable to delete image ' + imagePath);
+                                //   } else {
+                                //     console.log('image ' + imagePath + ' was deleted');
+                                //   }
+                                // });
+                            }
+                        });
+                    });
+                }
+            });
+        }
+    });
 }
 // Run the uploadRandomImage() method
 uploadRandomImage();
